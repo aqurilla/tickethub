d299ed7525eb0091891c6a2fe9a69d519dab8e52
Nitin Suresh <aqurilla@gmail.com>
2022-12-09 20:56:43 -0800

    setup test environment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/currentUser";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/errorHandler";
-import { NotFoundError } from "./errors/NotFoundError";
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', () => {
-    throw new NotFoundError();
-})
-
-// error-handling
-app.use(errorHandler);
+import { app } from "./app";
 
 const startUp = async () => {
     // check env variables
---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,48 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it('returns a 201 status on successful signup', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+});
+
+it('returns a 400 status with invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('returns a 400 status with invalid password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: '1'
+        })
+        .expect(400);
+});
+
+it('returns a 400 status with invalid body', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com'
+        })
+        .expect(400);
+
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            password: 'password'
+        })
+        .expect(400);
+});
