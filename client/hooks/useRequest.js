5c3aa1d1dc6d2d4b166fa61311ef0c3f7dcd26d2
Nitin Suresh <aqurilla@gmail.com>
2022-12-11 23:23:38 -0800

    refactor to use useRequest hook

---
+++ client/hooks/useRequest.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const useRequest = ({ url, method, body }) => {
+    const [errors, setErrors] = useState(null);
+    const doRequest = async () => {
+        try {
+            setErrors([]);
+            const response = await axios[method](url, body);
+            return response.data;
+        } catch (error) {
+            setErrors(<div className="alert alert-danger">
+                <h4>Errors:</h4>
+                <ul className="my-0">
+                    {error.response.data.errors.map((e, i) => <li key={i}>{e.message}</li>)}
+                </ul>
+            </div>);
+        }
+    }
+
+    return { doRequest, errors };
+}
+
+export default useRequest;
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,21 @@
 import { useState } from "react";
-import axios from "axios";
+import useRequest from "../../hooks/useRequest";
 
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email,
+            password
+        }
+    })
 
     const onSubmit = async (event) => {
         event.preventDefault();
-
-        try {
-            await axios.post('/api/users/signup', {
-                email, password
-            });
-            setErrors([]);
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
+        doRequest();
     }
 
     return (
@@ -37,13 +36,7 @@ const Signup = () => {
                     type="password"
                     className="form-control"></input>
             </div>
-            {errors.length > 0 && <div className="alert alert-danger">
-                <h4>Errors:</h4>
-                <ul className="my-0">
-                    {errors && errors.map((e, i) => <li key={i}>{e.message}</li>)}
-                </ul>
-            </div>}
-
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     )
