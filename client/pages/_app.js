a24d27f8c8fe3e7c57eccbcfdd8e2bb96dce8326
Nitin Suresh <aqurilla@gmail.com>
2022-12-15 00:18:06 -0800

    add header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.css'
+// this needs to be named _app.js
 
-const _app = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/buildClient';
+import { Header } from '../components/header';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} />
+        </div>
+    )
 }
 
-export default _app;
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+    const client = buildClient(ctx);
+    const { data } = await client.get('/api/users/currentuser');
+    let pageProps = {}
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return {
+        pageProps,
+        ...data
+    };
+}
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import buildClient from "../api/buildClient";
 
 const LandingPage = ({ currentUser }) => {
-    return <h1>Landing</h1>
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
 }
 
 LandingPage.getInitialProps = async (ctx) => {
