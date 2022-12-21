c3636747ad4bfbae648c69cdbe186f6d0c33c7fd
Nitin Suresh <aqurilla@gmail.com>
2022-12-20 20:58:57 -0800

    add ticketing service

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@nsth/common";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        // to enable cookie access during testing
+        secure: process.env.NODE_ENV != 'test'
+    })
+);
+
+app.all('*', () => {
+    throw new NotFoundError();
+})
+
+// error-handling
+app.use(errorHandler);
+
+export { app }
