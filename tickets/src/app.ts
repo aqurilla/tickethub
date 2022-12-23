59ed33d2e1fc6f102edc4574cf47dc73e4f26455
Nitin Suresh <aqurilla@gmail.com>
2022-12-22 22:27:02 -0800

    add getTicket route handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFoundError } from "@nsth/common";
 import { createTicketRouter } from "./routes/new";
+import { getTicketRouter } from "./routes/getTicket";
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(getTicketRouter);
 
 app.all('*', () => {
     throw new NotFoundError();
